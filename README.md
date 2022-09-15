1122982435c85c1fd791335da3fb68d71d89f1c5
Ivan Andreichuk <93861097+xmzboy@users.noreply.github.com>
2022-09-15 14:59:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker_flask_app
+ Simple flask app with docker
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    url = 'https://thecatapi.com/api/images/get'
+    return render_template("index.html", url=url)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
